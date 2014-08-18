0144c4cbc8f8dbad09c83b4b4f4aae89657a0df5
Alex Rønne Petersen <alex@lycus.org>
2014-08-18 10:54:46 +0200

    Sanitize the `string` implementation.

--- lib/ex_parsec/base.ex
+++ lib/ex_parsec/base.ex
@@ -512,25 +512,21 @@ defmodule ExParsec.Base do
     """
     @spec string(String.t()) :: ExParsec.t(term(), String.t())
     defparser string(string) in p do
-        sz = length(String.codepoints((string)))
-
-        loop = fn(loop, accp, acc) ->
-            cond do
-                acc == string -> success(accp, acc)
-                length(String.codepoints(acc)) >= sz ->
-                    failure([error(p, "expected #{inspect(string)} but found #{inspect(acc)}")])
-                true ->
-                    r = any_char().(accp)
+        loop = fn(loop, p, str) ->
+            case String.next_codepoint(str) do
+                {cp, str} ->
+                    r = char(cp).(p)
 
                     if r.status == :ok do
-                        loop.(loop, r.parser, acc <> r.result)
+                        loop.(loop, r.parser, str)
                     else
                         r
                     end
+                nil -> success(p, string)
             end
         end
 
-        loop.(loop, p, "")
+        loop.(loop, p, string)
     end
 
     @doc """
