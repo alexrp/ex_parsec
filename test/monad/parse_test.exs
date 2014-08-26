ca3e72baba5f5d47103620c05b266c9405a6bbc7
Alex Rønne Petersen <alex@lycus.org>
2014-08-26 08:38:15 +0200

    Add some tests for the monadic syntax.

---
+++ test/monad/parse_test.exs
@@ -0,0 +1,57 @@
+defmodule Test.ExParsec.Monad.Parse do
+    use ExUnit.Case, async: true
+
+    import ExParsec.Base
+    import ExParsec.Text
+
+    require ExParsec.Monad.Parse
+
+    alias ExParsec.Input.MemoryInput
+    alias ExParsec.Monad.Parse
+    alias ExParsec.Parser
+    alias ExParsec.Reply
+
+    test "successful binding" do
+        value = "x"
+        input = %MemoryInput{value: value}
+        parser = %Parser{input: input}
+
+        p = Parse.m do
+            x <- any_char()
+            return x
+        end
+
+        assert %Reply{status: :ok} = p.(parser)
+    end
+
+    test "successful let binding" do
+        value = ""
+        input = %MemoryInput{value: value}
+        parser = %Parser{input: input}
+
+        p = Parse.m do
+            let x = "foo"
+            let do
+                y = "bar"
+                z = "baz"
+            end
+            return x <> y <> z
+        end
+
+        assert %Reply{status: :ok} = p.(parser)
+    end
+
+    test "unsuccessful binding" do
+        value = "xz"
+        input = %MemoryInput{value: value}
+        parser = %Parser{input: input}
+
+        p = Parse.m do
+            x <- char("x")
+            y <- char("y")
+            return x <> y
+        end
+
+        assert %Reply{status: :error} = p.(parser)
+    end
+end
