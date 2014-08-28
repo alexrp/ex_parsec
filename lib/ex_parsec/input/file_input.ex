0a61090bd07ad2ed2d2c105dcaaf1199e054e209
Alex Rønne Petersen <alex@lycus.org>
2014-08-28 11:31:08 +0200

    Fix position backtracking in `FileInput`.

--- lib/ex_parsec/input/file_input.ex
+++ lib/ex_parsec/input/file_input.ex
@@ -3,14 +3,17 @@ defmodule ExParsec.Input.FileInput do
     Provides data from an I/O device in UTF-8 mode.
 
     * `device` is the I/O device.
+    * `position` is the current position in the device.
     """
 
-    defstruct device: nil
+    defstruct device: nil,
+              position: 0
 
     @typedoc """
     The type of an `ExParsec.Input.FileInput` instance.
     """
-    @type t() :: %__MODULE__{device: File.io_device()}
+    @type t() :: %__MODULE__{device: File.io_device(),
+                             position: non_neg_integer()}
 
     @doc """
     Checks if `value` is an `ExParsec.Input.FileInput` instance.
@@ -27,12 +30,14 @@ defimpl ExParsec.Input, for: ExParsec.Input.FileInput do
     @spec get(FileInput.t(), Keyword.t()) :: {FileInput.t(), String.codepoint()} |
                                              {:error, term()} | :eof
     def get(input, _) do
+        {:ok, pos} = :file.position(input.device, input.position)
+
         case IO.read(input.device, 1) do
             {:error, r} -> {:error, r}
             :eof -> :eof
             cp ->
                 if String.valid_character?(cp) do
-                    {input, cp}
+                    {%FileInput{input | :position => pos + byte_size(cp)}, cp}
                 else
                     {:error, :noncharacter}
                 end
